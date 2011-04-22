8993f07bd164dbf6727423fbdd44c790b044060d
crynobone <crynobone@gmail.com>
2011-04-22 21:54:01 +0800

    Standardize output from Oil

--- classes/generate.php
+++ classes/generate.php
@@ -97,7 +97,7 @@ CONF;
 
 		if (!$overwrite && is_file($path))
 		{
-			throw new Exception("app/config/{$file}.php already exist, please use -overwrite option to force update");
+			throw new Exception("APPPATH/config/{$file}.php already exist, please use -overwrite option to force update");
 		}
 		
 		$path = pathinfo($path);
@@ -105,11 +105,11 @@ CONF;
 		try
 		{
 			\File::update($path['dirname'], $path['basename'], $content);
-			\Cli::write("app/config/{$file}.php created.", 'green');
+			\Cli::write("Created config: APPPATH/config/{$file}.php", 'green');
 		}
 		catch (\File_Exception $e)
 		{
-			throw new Exception("app/config/{$file}.php could not be written.");
+			throw new Exception("APPPATH/config/{$file}.php could not be written.");
 		}
 	}
 
