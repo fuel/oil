e8f5b6339be86e72278b7baf20e802c62d455cfa
Dan Horrigan <dan@dhorrigan.com>
2011-10-08 19:46:13 -0700

    Fixing a PHP warning

--- classes/scaffold.php
+++ classes/scaffold.php
@@ -60,7 +60,8 @@ class Scaffold
 		$data['controller'] = 'Controller_'.\Inflector::pluralize(\Inflector::classify($full_underscores));
 
 		// If a folder is used, the entity is the last part
-		$data['singular'] = $singular = \Inflector::singularize(end(explode(DS, $full_thing)));
+		$parts = explode(DS, $full_thing);
+		$data['singular'] = $singular = \Inflector::singularize(end($parts));
 		$data['model'] = $model_name = 'Model_'.\Inflector::classify($full_underscores);
 		$data['plural'] = $plural = \Inflector::pluralize($singular);
 		$data['fields'] = $fields;
