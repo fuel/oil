ca2e281070d4a7185d81c30e7069f7fb23875194
WanWizard <wanwizard@wanwizard.eu>
2013-03-26 15:40:30 +0100

    bootstrap fix of the scaffold template
    thanks to tf2ftw

--- views/scaffolding/template.php
+++ views/scaffolding/template.php
@@ -10,28 +10,26 @@
 </head>
 <body>
 	<div class="container">
-		<div class="row">
-			<div class="span16">
-				<h1><?php echo $title; ?></h1>
-				<hr>
+		<div class="span12">
+			<h1><?php echo $title; ?></h1>
+			<hr>
 <?php if (Session::get_flash('success')): ?>
-				<div class="alert-message success">
-					<p>
-					<?php echo implode('</p><p>', e((array) Session::get_flash('success'))); ?>
-					</p>
-				</div>
+			<div class="alert-message success">
+				<p>
+				<?php echo implode('</p><p>', e((array) Session::get_flash('success'))); ?>
+				</p>
+			</div>
 <?php endif; ?>
 <?php if (Session::get_flash('error')): ?>
-				<div class="alert-message error">
-					<p>
-					<?php echo implode('</p><p>', e((array) Session::get_flash('error'))); ?>
-					</p>
-				</div>
+			<div class="alert-message error">
+				<p>
+				<?php echo implode('</p><p>', e((array) Session::get_flash('error'))); ?>
+				</p>
+			</div>
 <?php endif; ?>
-			</div>
-			<div class="span16">
+		</div>
+		<div class="span12">
 <?php echo $content; ?>
-			</div>
 		</div>
 		<footer>
 			<p class="pull-right">Page rendered in {exec_time}s using {mem_usage}mb of memory.</p>
