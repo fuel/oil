a2f2ede579736177eaf1feef3311b3e7363422e6
Sági-Kazár Márk <webmaster@firstcomputer.hu>
2013-09-07 21:20:51 +0200

    Templates updated to Bootstrap 3.0

--- views/admin/template.php
+++ views/admin/template.php
@@ -69,13 +69,17 @@
 <?php if (Session::get_flash('success')): ?>
 				<div class="alert alert-success alert-dismissable">
 					<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+					<p>
 					<?php echo implode('</p><p>', (array) Session::get_flash('success')); ?>
+					</p>
 				</div>
 <?php endif; ?>
 <?php if (Session::get_flash('error')): ?>
 				<div class="alert alert-error alert-dismissable">
 					<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+					<p>
 					<?php echo implode('</p><p>', (array) Session::get_flash('error')); ?>
+					</p>
 				</div>
 <?php endif; ?>
 			</div>
