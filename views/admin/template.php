7128977ba1bd551d085dff11c2092a09cdeec063
Amanda Tresbach <altfga@gmail.com>
2014-02-14 18:50:11 -0200

    rename css class "alert-error" to "alert-danger" the former was deprecated in Bootstrap 3
    Signed-off-by: Amanda Tresbach <altfga@gmail.com>

--- views/admin/template.php
+++ views/admin/template.php
@@ -75,7 +75,7 @@
 				</div>
 <?php endif; ?>
 <?php if (Session::get_flash('error')): ?>
-				<div class="alert alert-error alert-dismissable">
+				<div class="alert alert-danger alert-dismissable">
 					<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
 					<p>
 					<?php echo implode('</p><p>', (array) Session::get_flash('error')); ?>
