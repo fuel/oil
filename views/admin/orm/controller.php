f831ec9347e8aba9c73dbd13c4bf72846cc27036
kenjis <kenji.uui@gmail.com>
2014-04-12 17:00:57 +0900

    Fix line endings to be LF
    Signed-off-by: Kenji Suzuki <kenji.uui@gmail.com>

--- views/admin/orm/controller.php
+++ views/admin/orm/controller.php
@@ -1,12 +1,12 @@
 <?php echo '<?php' ?>
 
-class Controller_<?php echo $controller_name; ?> extends <?php echo \Cli::option('extends', $controller_parent).PHP_EOL; ?>
+class Controller_<?php echo $controller_name; ?> extends <?php echo \Cli::option('extends', $controller_parent)."\n"; ?>
 {
 
 <?php foreach ($actions as $action): ?>
 	public function action_<?php echo $action['name']; ?>(<?php echo $action['params']; ?>)
 	{
-<?php echo $action['code'].PHP_EOL; ?>
+<?php echo $action['code']."\n"; ?>
 	}
 
 <?php endforeach; ?>
