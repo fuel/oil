3cb4c72209d90af1164a780a38ecb8efe137d3ba
martonbaranyai <baranyaimarci92@gmail.com>
2015-02-25 00:18:53 +0100

    update bootstrap form-horizontal class markup, its just slighthly different, but they put in a new div around the form input elements
    Signed-off-by: martonbaranyai <baranyaimarci92@gmail.com>

--- views/admin/crud/views/actions/_form.php
+++ views/admin/crud/views/actions/_form.php
@@ -4,24 +4,29 @@
 	<fieldset>
 <?php foreach ($fields as $field): ?>
 		<div class="form-group">
-			<?php echo "<?php echo Form::label('". \Inflector::humanize($field['name']) ."', '{$field['name']}', array('class'=>'control-label')); ?>\n"; ?>
-
+			<?php echo "<?php echo Form::label('". \Inflector::humanize($field['name']) ."', '{$field['name']}', array('class'=>'col-sm-2 control-label')); ?>\n"; ?>
 <?php switch($field['type']):
 
 				case 'text':
-					echo "\t\t\t\t<?php echo Form::textarea('{$field['name']}', Input::post('{$field['name']}', isset(\${$singular_name}) ? \${$singular_name}->{$field['name']} : ''), array('class' => 'col-md-8 form-control', 'rows' => 8, 'placeholder'=>'".\Inflector::humanize($field['name'])."')); ?>\n";
+                                        echo "\t\t<div class='col-sm-10'>\n";
+					echo "\t\t\t\t<?php echo Form::textarea('{$field['name']}', Input::post('{$field['name']}', isset(\${$singular_name}) ? \${$singular_name}->{$field['name']} : ''), array('class' => 'form-control', 'rows' => 8, 'placeholder'=>'".\Inflector::humanize($field['name'])."')); ?>\n";
+                                        echo "\t\t</div>\n";
 				break;
 
 				default:
-					echo "\t\t\t\t<?php echo Form::input('{$field['name']}', Input::post('{$field['name']}', isset(\${$singular_name}) ? \${$singular_name}->{$field['name']} : ''), array('class' => 'col-md-4 form-control', 'placeholder'=>'".\Inflector::humanize($field['name'])."')); ?>\n";
+                                        echo "\t\t<div class='col-sm-4'>\n";
+					echo "\t\t\t\t<?php echo Form::input('{$field['name']}', Input::post('{$field['name']}', isset(\${$singular_name}) ? \${$singular_name}->{$field['name']} : ''), array('class' => 'form-control', 'placeholder'=>'".\Inflector::humanize($field['name'])."')); ?>\n";
+                                        echo "\t\t</div>\n";
 
 endswitch; ?>
+                        
 
 		</div>
 <?php endforeach; ?>
 		<div class="form-group">
-			<label class='control-label'>&nbsp;</label>
-			<?php echo '<?php'; ?> echo Form::submit('submit', 'Save', array('class' => 'btn btn-primary')); <?php echo '?>'; ?>
+                        <div class="col-sm-offset-2 col-sm-10">
+                                <?php echo '<?php'; ?> echo Form::submit('submit', 'Save', array('class' => 'btn btn-primary')); <?php echo '?>'; ?>
+                        </div>
 		</div>
 	</fieldset>
 <?php if ($csrf): ?>
