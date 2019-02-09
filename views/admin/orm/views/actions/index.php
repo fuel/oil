87ca140481b04f19c79465fcb6c39fe7575b4693
Liffindra Angga Zaaldian <liffindra.zaaldian@icloud.com>
2019-02-10 01:41:34 +0700

    Make table view responsive

--- views/admin/orm/views/actions/index.php
+++ views/admin/orm/views/actions/index.php
@@ -2,34 +2,36 @@
 <br>
 <?php echo "<?php if (\${$plural_name}): ?>"; ?>
 
-<table class="table table-striped">
-	<thead>
-		<tr>
-<?php foreach ($fields as $field): ?>
-			<th><?php echo \Inflector::humanize($field['name']); ?></th>
-<?php endforeach; ?>
-			<th></th>
-		</tr>
-	</thead>
-	<tbody>
-<?php echo '<?php'; ?> foreach ($<?php echo $plural_name; ?> as $item): <?php echo '?>'; ?>
-		<tr>
+<div class="table-responsive">
+	<table class="table table-striped">
+		<thead>
+			<tr>
+	<?php foreach ($fields as $field): ?>
+				<th><?php echo \Inflector::humanize($field['name']); ?></th>
+	<?php endforeach; ?>
+				<th></th>
+			</tr>
+		</thead>
+		<tbody>
+	<?php echo '<?php'; ?> foreach ($<?php echo $plural_name; ?> as $item): <?php echo '?>'; ?>
+			<tr>
 
-<?php foreach ($fields as $field): ?>
-			<td><?php echo '<?php'; ?> echo $item<?php echo '->'.$field['name']; ?>; <?php echo '?>'; ?></td>
-<?php endforeach; ?>
-			<td>
-				<?php echo '<?php'; ?> echo Html::anchor('<?php echo $uri; ?>/view/'.$item->id, 'View', array('class' => 'btn btn-xs btn-link')); <?php echo '?>'; ?>
-				<?php echo '<?php'; ?> echo Html::anchor('<?php echo $uri; ?>/edit/'.$item->id, 'Edit', array('class' => 'btn btn-xs btn-link')); <?php echo '?>'; ?>
-				<?php echo '<?php'; ?> echo Html::anchor('<?php echo $uri; ?>/delete/'.$item->id, 'Delete', array('class' => 'btn btn-xs btn-link', 'onclick' => "return confirm('Are you sure?')")); <?php echo '?>'; ?>
+	<?php foreach ($fields as $field): ?>
+				<td><?php echo '<?php'; ?> echo $item<?php echo '->'.$field['name']; ?>; <?php echo '?>'; ?></td>
+	<?php endforeach; ?>
+				<td>
+					<?php echo '<?php'; ?> echo Html::anchor('<?php echo $uri; ?>/view/'.$item->id, 'View', array('class' => 'btn btn-xs btn-link')); <?php echo '?>'; ?>
+					<?php echo '<?php'; ?> echo Html::anchor('<?php echo $uri; ?>/edit/'.$item->id, 'Edit', array('class' => 'btn btn-xs btn-link')); <?php echo '?>'; ?>
+					<?php echo '<?php'; ?> echo Html::anchor('<?php echo $uri; ?>/delete/'.$item->id, 'Delete', array('class' => 'btn btn-xs btn-link', 'onclick' => "return confirm('Are you sure?')")); <?php echo '?>'; ?>
 
 
-			</td>
-		</tr>
-<?php echo '<?php endforeach; ?>'; ?>
+				</td>
+			</tr>
+	<?php echo '<?php endforeach; ?>'; ?>
 
-	</tbody>
-</table>
+		</tbody>
+	</table>
+</div>
 
 <?php echo '<?php'; ?> echo $pagination <?php echo '?>'; ?>
 
