c5c57f1b84b999518f2b6cbd76c2bef1cce15673
Liffindra Angga Zaaldian <liffindra.zaaldian@icloud.com>
2019-02-09 22:59:13 +0700

    Use margin-top instead of margin for better display on phone

--- views/admin/template.php
+++ views/admin/template.php
@@ -5,7 +5,7 @@
 	<title><?php echo $title; ?></title>
 	<?php echo Asset::css('bootstrap.css'); ?>
 	<style>
-		body { margin: 50px; }
+		body { margin-top: 50px; }
 	</style>
 	<?php echo Asset::js(array(
 		'http://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js',
