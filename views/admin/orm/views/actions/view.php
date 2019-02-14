f567db4cb4afd884b79e6df5b0ea542a58f5d9ba
Liffindra Angga Zaaldian <liffindra.zaaldian@icloud.com>
2019-02-14 22:33:31 +0700

    Beautify view details page for admin scaffold

--- views/admin/orm/views/actions/view.php
+++ views/admin/orm/views/actions/view.php
@@ -1,11 +1,15 @@
 <h2>Viewing #<?php echo '<?php'; ?> echo $<?php echo $singular_name; ?>->id; <?php echo '?>'; ?></h2>
+<br>
 
+<dl class="dl-horizontal">
 <?php foreach ($fields as $field): ?>
-<p>
-	<strong><?php echo \Inflector::humanize($field['name']); ?>:</strong>
-	<?php echo '<?php'; ?> echo $<?php echo $singular_name.'->'.$field['name']; ?>; <?php echo '?>'; ?>
-</p>
+	<dt><?php echo \Inflector::humanize($field['name']); ?></dt>
+	<dd><?php echo '<?php'; ?> echo $<?php echo $singular_name.'->'.$field['name']; ?>; <?php echo '?>'; ?></dd>
+	<br>
 <?php endforeach; ?>
+</dl>
 
-<?php echo '<?php'; ?> echo Html::anchor('<?php echo $uri ?>/edit/'.$<?php echo $singular_name; ?>->id, 'Edit'); <?php echo '?>'; ?> |
-<?php echo '<?php'; ?> echo Html::anchor('<?php echo $uri ?>', 'Back'); <?php echo '?>'; ?>
+<div class="btn-group">
+	<?php echo '<?php'; ?> echo Html::anchor('<?php echo $uri ?>/edit/'.$<?php echo $singular_name; ?>->id, 'Edit', array('class' => 'btn btn-warning')); <?php echo '?>'.PHP_EOL; ?>
+	<?php echo '<?php'; ?> echo Html::anchor('<?php echo $uri ?>', 'Back', array('class' => 'btn btn-default')); <?php echo '?>'.PHP_EOL; ?>
+</div>
